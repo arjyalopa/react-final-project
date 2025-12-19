a41fce80f8bbf0cb1db448e4dcd00a622b8609d8
arjyalopa <arjyalopaa@gmail.com>
2025-12-19 16:16:53 +0530

    Add App.jsx with Paradise Nursery landing page
    Create App.jsx for Paradise Nursery landing page and user entry point

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Welcome to Paradise Nursery</h1>
+
+      <button
+        style={{
+          padding: "12px 24px",
+          fontSize: "16px",
+          cursor: "pointer",
+          marginTop: "20px"
+        }}
+      >
+        Get Started
+      </button>
+    </div>
+  );
+}
+
+export default App;
