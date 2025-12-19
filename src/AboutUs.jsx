b935b2525ffd9bdfff7d99b08fa7802c8c99938b
arjyalopa <arjyalopaa@gmail.com>
2025-12-19 15:17:58 +0530

    Add AboutUs component with nursery information
    This commit introduces an AboutUs component to the project, designed to display nursery-related information.

---
+++ src/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is dedicated to providing high-quality plants and
+        gardening solutions. Our mission is to promote green living and bring
+        nature closer to people.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
